00b24d64af4a2df90cb702602561a198f31cef07
Bryanchriswhite <bryanchriswhite@gmail.com>
2012-11-14 05:00:13 -0500

    rewrite mods overview feature to more accurately test expected results

--- spec/acceptance/mods_overview.feature
+++ spec/acceptance/mods_overview.feature
@@ -11,6 +11,12 @@ Feature: Overview Of Available Mods
       | world generation |
       | add-ons          |
       | item only        |
+      ## HOW ABOUT THIS? --v
+      #| id | name             |
+      #| 1  | full conversion  |
+      #| 2  | world generation |
+      #| 3  | add-ons          |
+      #| 4  | item only        |
     And the following mods:
       | id | name          | rating    | downloads |
       | 1  | red power 2   | 5         | 10000     |
@@ -19,19 +25,31 @@ Feature: Overview Of Available Mods
       | 4  | forestry      | 3.125     | 1234      |
       | 5  | MFFS          | 1         | 12000     |
       | 6  | IC2           | 2         | 200       |
-      | 7  | facrotization | 3         | 16161     |
+      | 7  | factorization | 3         | 16161     |
       | 8  | buildcraft    | 3         | 2222      |
       | 9  | railcraft     | 4.9       | 545455    |
       | 10 | portal gun    | 5         | 42000     |
     When I visit the home page
     Then I should see the five most popular mods links:
+      | text          | path     |
+      | red power 2   | /mods/1  |
+      | railcraft     | /mods/9  |
+      | portal gun    | /mods/10 |
+      | factorization | /mods/7  |
+      | MFFS          | /mods/5  |
+    And I should see the five highest rated mods links
       | text        | path     |
       | red power 2 | /mods/1  |
-      | mystcraft   | /mods/2  |
-      | portal gun  | /mods/10 |
       | railcraft   | /mods/9  |
+      | portal gun  | /mods/10 |
+      | mystcraft   | /mods/2  |
       | gregtech    | /mods/3  |
-    And I should see the five highest rated mods links
     And I should see a link to the highest rated mods list
     And I should see a link to the most popular mods list
     And I should see a link for each category
+      ## HOW ABOUT THIS? --v
+      #| path           | name             |
+      #| /categories/1  | full conversion  |
+      #| /categories/2  | world generation |
+      #| /categories/3  | add-ons          |
+      #| /categories/4  | item only        |
--- spec/acceptance/steps/mod_overview_steps.rb
+++ spec/acceptance/steps/mod_overview_steps.rb
@@ -8,7 +8,7 @@ step "the following mods:" do |table|
   table.hashes.each do |hash|
     FactoryGirl.build :mod do |mod|
       mod.attributes = hash
-      mod.id = hash[:id]
+      mod.id = hash['id']
       mod.save
     end
   end
@@ -20,17 +20,47 @@ end
 
 step "I should see the five most popular mods links:" do |table|
   count = 0
-
+  five_most_popular = page.find '.five_most_popular'
   table.hashes.each do |hash|
-    binding.pry
-    page.should have_xpath %Q^//*[@class='five_most_popular']//a[@href="#{hash['path']}"]^, :text => hash['text']#, :count => 5
+
+    ## OLD, SLIGHTLY LESS TERRIBLE WAY TO FIND DESCENDANT, RELATIVE LINKS --v
+    #five_most_popular.should have_xpath %Q^.//a[@href="#{hash['path']}"]^, :text => hash['text']
+    #
+    ## NEW AND IMPROVED WAY XD  --v
+    five_most_popular.should have_link hash['text'], href: hash['path']
     count += 1
   end
-
   count.should == 5
 end
 
-step "I should see the five highest rated mods links" do
-  links_array = find(".highest_rated").all("a")
-  links_array.should have(5).items
+step "I should see the five highest rated mods links" do |table|
+  count = 0
+  five_highest_rated = page.find '.five_highest_rated'
+  table.hashes.each do |hash|
+
+    ## OLD, SLIGHTLY LESS TERRIBLE WAY TO FIND DESCENDANT, RELATIVE LINKS --v
+    #five_highest_rated.should have_xpath %Q^.//a[@href="#{hash['path']}"]^, :text => hash['text']
+    #
+    ## NEW AND IMPROVED WAY XD  --v
+    five_highest_rated.should have_link hash['text'], href: hash['path']
+    count += 1
+  end
+  count.should == 5
+end
+
+step "I should see a link to the highest rated mods list" do
+  pending 'waiting for tim'
+  page.should have_link 'Highest Rated', href: '/mods/by_rating/highest_first'
+end
+
+step "I should see a link to the most popular mods list" do
+  pending 'waiting for tim'
+  page.should have_link 'Highest Rated', href: '/mods/by_downloads/highest_first'
+end
+
+step "I should see a link for each category" do #|table|
+  pending 'waiting for tim'
+  #table.hashes.each do |hash|
+    #page.should have_link hash['name'], href: hash['path']
+  #end
 end
